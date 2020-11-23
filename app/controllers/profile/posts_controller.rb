a7a9c7785618ad4bdf4a0de2a1a87d4e6cb31138
jayzen <jayzen@foxmail.com>
2020-11-23 15:46:09 +0800

    fix profile post error

--- app/controllers/profile/posts_controller.rb
+++ app/controllers/profile/posts_controller.rb
@@ -1,5 +1,7 @@
 class Profile::PostsController < Profile::BaseController
   def index
-    @posts = @user.posts.order(id: :desc).page(params[:page])
+    #campo default topic被删除，显示对应的post会报错,报id显示错误
+    #@posts = @user.posts.order(id: :desc).page(params[:page])
+    @posts = @user.posts.includes(:topic).where(topics: { deleted_at: nil }).order(id: :desc).page(params[:page])
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < ApplicationMailer
+  default from: "zhengjiajun121@gmail.com"
+
   def password_reset
     @user = params[:user]
     mail(subject: 'Reset password', to: @user.email)
+  end
+
+  def welcome_email
+    mail(to: "645112281@qq.com", subject: "Welcome to My Awesome Site")
   end
 end
