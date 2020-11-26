2a225b7cd20972a3252dae4fee69c75988a8c45f
jayzen <jayzen@foxmail.com>
2020-11-26 21:13:45 +0800

    remove sidekiq file

--- app/controllers/profile/posts_controller.rb
+++ app/controllers/profile/posts_controller.rb
@@ -1,7 +1,7 @@
 class Profile::PostsController < Profile::BaseController
   def index
     #campo default topic被删除，显示对应的post会报错,报id显示错误
-    #@posts = @user.posts.order(id: :desc).page(params[:page])
-    @posts = @user.posts.includes(:topic).where(topics: { deleted_at: nil }).order(id: :desc).page(params[:page])
+    @posts = @user.posts.order(id: :desc).page(params[:page])
+    #@posts = @user.posts.includes(:topic).where(topics: { deleted_at: nil }).order(id: :desc).page(params[:page])
   end
 end
